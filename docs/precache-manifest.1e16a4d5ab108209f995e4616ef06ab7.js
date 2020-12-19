5edbc11a2450a89f4b417a6eb0652ccfcf8c09dd
James Pettit <james@academia.edu>
2020-12-18 20:18:39 -0800

    Build from master

---
+++ docs/precache-manifest.1e16a4d5ab108209f995e4616ef06ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2474bdbebbd6e9f6eb26e07cc305a440",
+    "url": "/papertype/index.html"
+  },
+  {
+    "revision": "f59cd62176cac9d23fbd",
+    "url": "/papertype/static/css/main.6339440a.chunk.css"
+  },
+  {
+    "revision": "8d14394ef440aeb8f4a4",
+    "url": "/papertype/static/js/2.d8812a06.chunk.js"
+  },
+  {
+    "revision": "0a7ee2bb1371cce183a034a5e05af75f",
+    "url": "/papertype/static/js/2.d8812a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59cd62176cac9d23fbd",
+    "url": "/papertype/static/js/main.21e918f8.chunk.js"
+  },
+  {
+    "revision": "1cef77c86a01b47754ed",
+    "url": "/papertype/static/js/runtime-main.43842e8d.js"
+  }
+]);
